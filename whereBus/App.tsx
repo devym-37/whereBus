558557ecad58a9154ce8648060ae6458c0e66671
miniyoung <miniyoung37@gmail.com>
2022-08-29 23:43:03 +0900

    02. react native vector icons 설정 및 bottom style 추가

--- whereBus/App.tsx
+++ whereBus/App.tsx
@@ -1,33 +1,65 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import AntDesignIcons from 'react-native-vector-icons/AntDesign';
+import FontAwesomeIcons from 'react-native-vector-icons/FontAwesome5';
 
 import Home from './src/views/Home';
 
-const Tab = createMaterialBottomTabNavigator();
+const Tab = createBottomTabNavigator();
 
 const BottomNavigator = () => {
   return (
     <Tab.Navigator
       initialRouteName="Home"
-      activeColor="#e91e63"
-      inactiveColor="black"
-      barStyle={styles.barStyle}>
+      screenOptions={{
+        tabBarActiveTintColor: '#e91e63',
+        headerShown: false,
+      }}>
       <Tab.Screen
         name="홈"
         component={Home}
         options={{
           tabBarLabel: '홈',
-          tabBarIcon: ({ color }) => (
-            <AntDesignIcons name="home" color={color} size={26} />
+          tabBarLabelStyle: { fontSize: 12 },
+          tabBarIcon: ({ color, size }) => (
+            <AntDesignIcons name="home" color={color} size={size} />
           ),
         }}
       />
-      <Tab.Screen name="정류장" component={Home} />
-      <Tab.Screen name="버스" component={Home} />
-      <Tab.Screen name="정보" component={Home} />
+      <Tab.Screen
+        name="정류장"
+        component={Home}
+        options={{
+          tabBarLabel: '정류장',
+          tabBarLabelStyle: { fontSize: 12 },
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesomeIcons name="map-marked-alt" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="버스"
+        component={Home}
+        options={{
+          tabBarLabel: '버스',
+          tabBarLabelStyle: { fontSize: 12 },
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesomeIcons name="bus" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="정보"
+        component={Home}
+        options={{
+          tabBarLabel: '정보',
+          tabBarLabelStyle: { fontSize: 12 },
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesomeIcons name="list-ul" color={color} size={size} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
@@ -39,11 +71,5 @@ const App = () => {
     </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  barStyle: {
-    backgroundColor: 'white',
-  },
-});
 
 export default App;
